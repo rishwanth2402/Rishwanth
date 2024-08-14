06f3f3f62a34c9f07118dab6c6e563058f04d38d
rishwanth2402 <rishwanthoggu123@gmail.com>
2024-08-14 10:43:55 +0530

    Delete app_server directory

--- app_server/routes/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrlMain = require('../controllers/main');
-/* GET homepage. */
-router.get('/', ctrlMain.index);
-module.exports = router;
